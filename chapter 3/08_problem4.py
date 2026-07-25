79b9b843126d18c2e3334c75244e10f85be61a5d
mddanish-31 <mddanishraza904@gmail.com>
2026-07-25 21:51:02 +0530

    Create 08_problem4.py

---
+++ chapter 3/08_problem4.py
@@ -0,0 +1,3 @@
+name="md danish raza is a  good  boy and he is an  engineering student"
+print(name.replace("  "," "))
+print(name) #Strings are immutable which mean that you cannot change them ny running function on them
