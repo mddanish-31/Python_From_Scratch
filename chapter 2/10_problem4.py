46227f9241fb4877834da583ed27204844d2abdd
mddanish-31 <mddanishraza904@gmail.com>
2026-07-25 21:50:45 +0530

    Create 10_problem4.py

---
+++ chapter 2/10_problem4.py
@@ -0,0 +1,19 @@
+a=34
+b=80
+print(a>b)# if a is greater than b then return true else return false
+# return false
+
+a=34
+b=80
+print(a<b)# if a is less than b then return true else return false
+# return true
+
+a=34
+b=80
+print(a<=b)# if a is less than or equal to b then return true else return false
+# return true
+
+a=34
+b=80
+print(a>=b)# if a is greater than or equal to b then return true else return false
+# return false
