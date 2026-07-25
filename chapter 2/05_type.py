a87f3c1ce8b7787eb1c4caa87e92d91e23d6ef07
mddanish-31 <mddanishraza904@gmail.com>
2026-07-25 17:32:53 +0530

    Create 05_type.py

---
+++ chapter 2/05_type.py
@@ -0,0 +1,22 @@
+a=10 # class integer type
+t=type(a)
+print(t)
+
+b=20.90 # class float type
+t=type(b)
+print(t)
+
+c="DANISH" # class Integer type
+t=type(c)
+print(t)
+
+
+# Type Casting
+a=10
+b=float(a)
+print(b)
+
+
+a=10.0
+b=int(a)
+print(b)
