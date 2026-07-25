15848c8069b6a46112c61f2fe0e16ccbc80b94bf
mddanish-31 <mddanishraza904@gmail.com>
2026-07-25 17:32:50 +0530

    Create 04_operators.py

---
+++ chapter 2/04_operators.py
@@ -0,0 +1,28 @@
+# Arithmetic Operators
+a=10
+b=20
+c=a+b
+print(c)
+
+# Assignment Operator
+a=4-2 # assign 4-2 in a
+print(a)
+b=6
+b+=3 #Increment the value of b by 3 and then assign it to b
+c=5
+c-=3 #decrement the value of b by 3 and then assign it to c
+print(b)
+print(c)
+
+# Comparison Operator
+d=5>4
+print(d)
+e=5==5
+print(e)
+
+# Logical Operators
+
+f= True or False
+print("f:",f)
+
+print(not(False))
