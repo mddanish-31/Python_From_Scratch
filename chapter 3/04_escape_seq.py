b7ad732cdb3e14e2ead2b59ac88b80d6ec30b5f2
mddanish-31 <mddanishraza904@gmail.com>
2026-07-25 21:50:55 +0530

    Create 04_escape_seq.py

---
+++ chapter 3/04_escape_seq.py
@@ -0,0 +1,9 @@
+name="md danish raza is a engineering student\nfrom adamas university\nhaving a stable cgpa of 8.5\nafter completion of 2 years"
+print(name)
+print(name.title())
+
+name1="Adamas \tUniversity"
+print(name1)
+
+name2="\"Adamas\" \"University\""
+print(name2)
