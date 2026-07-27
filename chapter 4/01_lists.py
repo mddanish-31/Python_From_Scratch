df2b9104661aa1c693bd34237e9e88ab68e9382d
mddanish-31 <mddanishraza904@gmail.com>
2026-07-27 14:42:57 +0530

    Create 01_lists.py

---
+++ chapter 4/01_lists.py
@@ -0,0 +1,7 @@
+friends=["apple", "orange", 5, 345.06, False, "Aakash", "Rohan"]
+
+print(friends[0])
+friends[0]="grapes" #unlike strings lists are mutable 
+
+print(friends[0])
+print(friends[0:6])
