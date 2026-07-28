3f61fad0c5ae0556249d02e3ca989ba845ef5d9e
mddanish-31 <mddanishraza904@gmail.com>
2026-07-28 22:11:36 +0530

    Update 02_list_methods.py

--- chapter 4/02_list_methods.py
+++ chapter 4/02_list_methods.py
@@ -4,15 +4,21 @@ friends.append("danish")
 print(friends)
 
 l1=[1,23,45,32,2,45,67,76]
-l1.sort()
+l1.sort() #update the list
 print(l1)
-l1.insert(2,10)
+
+l1.insert(2,10) # this will insert 10 at index 2
 print(l1)
 
 l1=[1,23,45,32,2,45,67,76]
-l1.pop(4)
+l1.pop(4) # will delete element at index 4 and return the value
 print(l1)
 
 l1=[1,23,45,32,2,45,67,76]
-l1.remove(45)
+l1.remove(45) # remove 45 from the list which apprears first
+print(l1)
+
+
+l1=[1,23,45,32,2,45,67,76]
+l1.append(4) # add 4 at the end of the index
 print(l1)
