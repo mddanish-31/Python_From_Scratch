e699d0711a610645531e47a499dea3a14aa4b6df
mddanish-31 <mddanishraza904@gmail.com>
2026-07-29 11:36:46 +0530

    Create 04_tuple_methods.py

---
+++ chapter 4/04_tuple_methods.py
@@ -0,0 +1,19 @@
+a=(1,4,6,75,40,76,65,75,90,0) # a.count() will return number of times a number occur in a
+print(a.count(75))
+print(a.index(75)) # a.index() will return the index of first occurence of 1 in a
+print(len(a))
+
+
+tuple1 =(1,2,3,4,5)
+tuple2 =(6,7,8,9,10)
+result= tuple1+tuple2 # to join 2 or different types of tuple
+print(result)
+print(len(a))
+
+a=(1,2,3)
+# a,b,c=a
+print(a)
+
+a=(1,2,3)
+a,b,c=a
+print(a,b,c)
