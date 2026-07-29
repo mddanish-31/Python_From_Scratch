8258bb39f18a0eb891d330a98113aca477c59214
mddanish-31 <mddanishraza904@gmail.com>
2026-07-29 11:36:37 +0530

    Create 03_tuple.py

---
+++ chapter 4/03_tuple.py
@@ -0,0 +1,11 @@
+a=(1,4,6,75,40,76) # this is how a tuple is defined
+print(type(a))
+
+a=(1,)
+print(type(a))  #this is how we define a single tuple
+
+a=(1,4,6,75,40,76) # this is how we define more than 1 tuple
+print(type(a))
+
+
+
