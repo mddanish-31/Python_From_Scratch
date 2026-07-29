39b818861d4dc212845803fbc0a814a2184ea993
mddanish-31 <mddanishraza904@gmail.com>
2026-07-29 11:36:53 +0530

    Create 01_problem1.py

---
+++ chapter 4/Practice Set/01_problem1.py
@@ -0,0 +1,20 @@
+# write a program to store seven fruits in a list elements by the user
+fruits=[]
+
+f1=input("enter fruits name:") 
+fruits.append(f1)
+f2=input("enter fruits name:")
+fruits.append(f2)
+f3=input("enter fruits name:")
+fruits.append(f3)
+f4=input("enter fruits name:")
+fruits.append(f4)
+f5=input("enter fruits name:")
+fruits.append(f5)
+f6=input("enter fruits name:")
+fruits.append(f6)
+f7=input("enter fruits name:")
+fruits.append(f7)
+f8=input("enter fruits name:")
+fruits.append(f8)
+print(fruits)
