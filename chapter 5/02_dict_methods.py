29ae29be5e1ca34d2254a89fd46b404a995beee4
mddanish-31 <mddanishraza904@gmail.com>
2026-07-30 17:08:29 +0530

    Create 02_dict_methods.py

---
+++ chapter 5/02_dict_methods.py
@@ -0,0 +1,12 @@
+marks={
+    "Danish":100,
+    "Faizan":69,
+    "Nafis":0,
+    10:"Rajesh"
+}
+
+# print(marks.items()) #prints the dict of items
+# print(marks.keys()) #prints the keys name from the dict
+# print(marks.values(),type(marks)) #prints the values from the dict
+marks.update({"Danish":99})
+print(marks)
