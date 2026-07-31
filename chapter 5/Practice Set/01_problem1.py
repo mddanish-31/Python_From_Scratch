edd97ce15e808392030fa57a03e8c8745a03ef4e
mddanish-31 <mddanishraza904@gmail.com>
2026-07-31 12:52:44 +0530

    Create 01_problem1.py

---
+++ chapter 5/Practice Set/01_problem1.py
@@ -0,0 +1,17 @@
+# Write a program to create a dictionary of Hindi words with values 
+# as their English translation.
+# Provide user with an option to look it up!
+Dictionary={
+    "namaste": "Hello",
+    "pani": "Water",
+    "kitab": "Book",
+    "ghar": "House",
+    "dost": "Friend",
+    "khana": "Food",
+    "maa": "Mother",
+    "pita": "Father",
+    "bacha": "Child",
+    "ped": "Tree"
+}
+word=input("Enter the Hindi word to find its English meaning:")
+print("word in dictionary:-  ",Dictionary[word])
