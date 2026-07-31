5b5afbf5e8397dcd5c4b879197e56db68fa563f9
mddanish-31 <mddanishraza904@gmail.com>
2026-07-31 12:52:21 +0530

    Create 03_sets.py

---
+++ chapter 5/03_sets.py
@@ -0,0 +1,12 @@
+s=set() # repeatation of same number is not allowed
+s.add(1)
+s.add(2)
+print(s,type(s))
+
+
+# s={}
+# print(s,type(s))
+# print(s)
+
+e=set() # dont use s ={ as it will create an empty dictionary}
+print(e) 
