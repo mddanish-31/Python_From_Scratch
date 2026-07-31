01b6255ad231bba3f1d68cbda5580ef117f517ad
mddanish-31 <mddanishraza904@gmail.com>
2026-07-31 12:52:40 +0530

    Create 05_union_intersection.py

---
+++ chapter 5/05_union_intersection.py
@@ -0,0 +1,10 @@
+#   Returns a new set with all items from both sets using the union 
+# s1={1,2,3,4,5}
+# s2={6,7,8,9,10}
+# print(s1.union(s2))
+
+#   Returns a set which contains only item in both set
+s1={10,20,30,40,50}
+s2={10,40,50,60,70}
+print(s1.intersection(s2)) #    or
+print(s2.intersection(s1)) # returns the same value
