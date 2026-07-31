0bd65da295d64cb0211fd5462cd2f09a4ea780d7
mddanish-31 <mddanishraza904@gmail.com>
2026-07-31 12:53:17 +0530

    Create tempCodeRunnerFile.py

---
+++ chapter 5/Practice Set/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+name=input("enter friend name :-")
+# lang=input("enter language name:-")
+# d.update({name:lang})
+# name=input("enter friend name :-")
+# lang=input("enter language name:-")
+# d.update({name:lang})
