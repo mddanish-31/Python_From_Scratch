404f0aaf9f10d4f28942a2d1d7438b343de15496
mddanish-31 <mddanishraza904@gmail.com>
2026-07-31 12:52:49 +0530

    Create 02_problem2.py

---
+++ chapter 5/Practice Set/02_problem2.py
@@ -0,0 +1,21 @@
+#   Write a program to input eight  from the user and 
+#  display all the unique  (once).
+s=set()
+n=input("enter the number:-")
+s.add(int(n))
+n=input("enter the number:-")
+s.add(int(n))
+n=input("enter the number:-")
+s.add(int(n))
+n=input("enter the number:-")
+s.add(int(n))
+n=input("enter the number:-")
+s.add(int(n))
+n=input("enter the number:-")
+s.add(int(n))
+n=input("enter the number:-")
+s.add(int(n))
+n=input("enter the number:-")
+s.add(int(n))
+print(s) # since repeatation is not allowed in set so it will give unique numbers
+
