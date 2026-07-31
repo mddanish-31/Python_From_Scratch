175f987304a3a77b736196189791f9a7ea2d49f2
mddanish-31 <mddanishraza904@gmail.com>
2026-07-31 12:52:33 +0530

    Create 04_set_methods.py

---
+++ chapter 5/04_set_methods.py
@@ -0,0 +1,25 @@
+s={1,23,43,22,7,54,"Danish"}
+# print(s,type(s)) 
+
+
+# adding number in a set
+# s=set()
+# s.add(2)
+# s.add(20)
+# print(s)
+
+#counting length of a set
+# print(len(s))
+
+#removing a number from a set
+s.remove(7)
+print(s,type(s))
+
+# randomly removes any element from the set
+s.pop()
+print(s)
+
+# empty the set
+s.clear()
+print(s)
+
