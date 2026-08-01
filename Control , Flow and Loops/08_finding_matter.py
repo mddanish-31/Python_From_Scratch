82936bb994c35e480b11a587f69d7cf039a4e0fd
mddanish-31 <mddanishraza904@gmail.com>
2026-08-01 22:32:23 +0530

    Create 08_finding_matter.py

---
+++ Control , Flow and Loops/08_finding_matter.py
@@ -0,0 +1,7 @@
+# Write a program to find out whether a given post is talking about “Danish” or not.
+a="Danish"
+post=input("Enter the post: ")
+if a in post:
+    print("The post is talking about Danish")
+else:
+    print("The post is not talking about Danish")
