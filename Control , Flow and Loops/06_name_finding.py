b6e3646b7dd35f1bf95e96a431854cfba27c2e5a
mddanish-31 <mddanishraza904@gmail.com>
2026-08-01 22:32:13 +0530

    Create 06_name_finding.py

---
+++ Control , Flow and Loops/06_name_finding.py
@@ -0,0 +1,6 @@
+name_list=["Danish", "Atiya", "Faizan", "Nafis", "Mustafa","Zuhi","Sriza","Sristi"]
+name=input("Enter the name to be searched: ")
+if name in name_list:
+    print("Name is present in the list")
+else:
+    print("Name is not present in the list")
