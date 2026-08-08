8960e9feaac2bdc8268712fd2934fb91502a415f
mddanish-31 <mddanishraza904@gmail.com>
2026-08-08 11:44:15 +0530

    Update 18_star_problem.py

--- Control , Flow and Loops/18_star_problem.py
+++ Control , Flow and Loops/18_star_problem.py
@@ -7,5 +7,6 @@ for n = 3  '''
 
 n=int(input("enter the number"))
 for i in range(1,n+1):
- print(" "*(n-i),end=" ")
- print("*"*(2*n-i),end=" ")
+ print(" " * (n-i),end="")
+ print("*" * (2*i-1),end="")
+ print("")
