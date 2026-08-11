88e5ca2c926b77f5e995f857afccba90b8d921e5
mddanish-31 <mddanishraza904@gmail.com>
2026-08-11 11:48:55 +0530

    Create 01_functions.py

---
+++ Chapter 8/01_functions.py
@@ -0,0 +1,7 @@
+def avg(): #function definition
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    c = float(input("Enter third number: "))
+    average = (a + b + c) / 3
+    print("The average of the three numbers is:", average)
+avg()  #function call
