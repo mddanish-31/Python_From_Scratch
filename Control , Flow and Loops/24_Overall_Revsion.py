b233edb275a2aab1155c7cc36eeb78ebb893dd9c
mddanish-31 <mddanishraza904@gmail.com>
2026-08-13 16:27:11 +0530

    Create 24_Overall_Revsion.py

---
+++ Control , Flow and Loops/24_Overall_Revsion.py
@@ -0,0 +1,59 @@
+# Example 1-- vowel or not
+# a=input("enter the character ")
+# if((a=='a') or (a=='e') or (a=='i') or (a=='o') or (a=='u')):
+#  print("the given character is a vowel ")
+# else:
+#     print("the given character is not a vowel ")
+
+
+# Example 2-- given number is less than 5.if yes then print the square of it
+# a=int(input("enter the number "))
+# if(a<5 or a<=5):
+#     print("the square of the number is: ",a*a)
+# else:
+#     print("the number is greater than 5")
+
+
+# Example 3-- number divisble by 10
+# a=int(input("enter the number "))
+# if(a%10)==0:
+#     print("number is divisble by 10")
+# else:
+#     print("number is not divisble by 10")
+    
+
+# Example 4--count the number of 100 rupee note required for a given amount and 
+# print whether or not more than one 100 rupee not is required
+# a=int(input("enter the amount:"))
+# x=a/100
+# if(x>1):
+#     print("more than one 100 rupee not is required:",x)
+# else:
+#     print("1 hundred rupee note is required")
+
+
+# Example 5--number>5 print cube
+#          --number<=5 print square 
+
+# a=int(input("enter the number "))
+# if(a<5 or a<=5):
+#     print("the square of the number is: ",a*a)
+# else:
+#     print(a*a*a)
+    
+
+# Example 6---profit or loss
+# a=int(input("enter selling price"))
+# b=int(input("enter cost price"))
+# if(b<a):
+#     print("profit")
+# else:
+#     print("loss")
+
+
+# Example 7--given character is in uppercase or lowercase
+# a=input("enter the character ")
+# if(a.isupper()):
+#     print("chacacter is upper case")
+# else:
+#     print("character is lower case")
